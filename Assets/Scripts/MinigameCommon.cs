b12a726fe2efdde7e2df554a9780cafc80a644a5
Sam Gates <skiracer1292@gmail.com>
2020-04-18 00:36:56 -0400

    Starting to work on common functions

---
+++ Assets/Scripts/MinigameCommon.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigameCommon : MonoBehaviour
+{
+    //GameObject RaycastFromMouseForTag(string tag)
+    //{
+    //    Camera cam = Camera.main;
+    //    Ray r = cam.ScreenPointToRay(Input.mousePosition);
+        
+    //}
+}
