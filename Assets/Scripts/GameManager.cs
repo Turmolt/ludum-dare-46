34f7707d15517a384ef8c108e074515128dfbcbd
Sam Gates <skiracer1292@gmail.com>
2020-04-17 23:53:01 -0400

    initial commit 2

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using Boo.Lang;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public string[] Minigames;
+
+    private Minigame activeMinigame;
+
+    private string activeSceneName;
+
+    private List<string> bagOfGames;
+
+    void Start()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            instance = this;
+        }
+        else if(instance!=this)
+        {
+            Destroy(this.gameObject);
+        }
+
+        bagOfGames = new List<string>();
+    }
+
+    public void SetActiveMinigame(Minigame game)
+    {
+        this.activeMinigame = game;
+    }
+
+    /// <summary>
+    /// Pulls random scene from MinigameScenes and loads it
+    /// </summary>
+    public void LoadRandomScene()
+    {
+        var selectedGame = PopMinigame();
+
+        if (activeSceneName == selectedGame)
+        {
+            var returningGame = selectedGame;
+
+            selectedGame = PopMinigame();
+            
+            bagOfGames.Add(returningGame);
+        }
+
+        StartCoroutine(LoadSceneAsync(selectedGame));
+    }
+
+    IEnumerator LoadSceneAsync(string sceneName)
+    {
+        //unload previous scene if there is one
+        if (!string.IsNullOrEmpty(activeSceneName))
+        {
+            AsyncOperation asyncUnload = SceneManager.UnloadSceneAsync(activeSceneName);
+            yield return new WaitUntil(()=>asyncUnload.isDone);
+        }
+
+        //load next scene
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+        yield return new WaitUntil(()=>asyncLoad.isDone);
+
+        //set active scene name
+        activeSceneName = sceneName;
+    }
+
+    string PopMinigame()
+    {
+        if (bagOfGames.Count == 0)
+        {
+            LoadBag();
+        }
+        
+        var index = Random.Range(0, bagOfGames.Count);
+        var selected = bagOfGames[index];
+        bagOfGames.Remove(selected);
+        return selected;
+    }
+
+    void LoadBag()
+    {
+        bagOfGames.AddRange(Minigames);
+    }
+}
---
+++ Assets/Scripts/Minigame.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public abstract class Minigame : MonoBehaviour
+{
+    void Awake()
+    {
+        GameManager.instance.SetActiveMinigame(this);
+    }
+
+    protected int difficulty;
+
+    public virtual void Setup(int difficulty) => this.difficulty = difficulty;
+
+    public virtual void StartGame(){}
+
+    public virtual void Pause() { }
+
+    public virtual void Cleanup() { }
+
+    public Action OnGameWin { get; set; }
+
+    public Action OnGameLose { get; set; }
+}
