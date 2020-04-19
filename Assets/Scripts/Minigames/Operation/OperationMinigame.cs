2bc1f14be06eade94caba86e6707819ce60708c8
Jonathan Cole <joncole04412@gmail.com>
2020-04-18 23:02:51 -0400

    Iterate

--- Assets/Scripts/Minigames/Operation/OperationMinigame.cs
+++ Assets/Scripts/Minigames/Operation/OperationMinigame.cs
@@ -5,7 +5,18 @@ using UnityEngine;
 namespace CheeseTeam{
     
     public class OperationMinigame : Minigame {
+
+        public GameObject organPrefab;
         private DraggableObject grabbedObject;
+
+        private List<Organ> organs = new List<Organ>();
+
+        public override void StartGame() {
+            for (int i = 0; i < 5; i++) {
+                var organ = MakeOrgan("Organ " + i.ToString(), Vector3.zero);
+                organs.Add(organ);
+            }
+        }
 
         void Update() {
             if (Input.GetMouseButtonDown(0)) {
@@ -19,6 +30,13 @@ namespace CheeseTeam{
                 grabbedObject.InHand = false;
                 grabbedObject = null;
             }
+        }
+
+        Organ MakeOrgan(string name, Vector3 pos) {
+            var obj = Instantiate(organPrefab, pos, Quaternion.identity);
+            obj.name = name;
+            obj.GetComponent<DragTag>().id = name;
+            return obj.GetComponent<Organ>();
         }
 
     }
